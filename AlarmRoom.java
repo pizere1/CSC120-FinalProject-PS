0415e7cc185e2238e223d2eb48c5afbfd923c8f3
Swikriti Dumre <sdumre@smith.edu>
2024-12-10 12:07:15 -0500

    with sec guard

--- AlarmRoom.java
+++ AlarmRoom.java
@@ -3,7 +3,20 @@ import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 
+/**
+ * A room in front of the escape room
+ * just pressurizes the 
+ */
 public class AlarmRoom {
+
+    public AlarmRoom(Securityguard sguard) {
+    }
+
+    public void warning(){
+        Securityguard sguard = new Securityguard("Swikriti", 21);
+        System.out.println("The Alarm has gone off. The Security Guards are after you. Find your escape quick before they catch you.");
+        sguard.catchPlayer();
+    }
 
     public void playSound() {
         try {
@@ -16,5 +29,5 @@ public class AlarmRoom {
             ex.printStackTrace();
         }
     }
-    
+
 }
