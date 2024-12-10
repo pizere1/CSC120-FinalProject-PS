1a7a8f5e1c10f9f6aa1821f104026543eb974fa0
Peace Izere <pizere@smith.edu>
2024-12-10 11:53:41 -0500

    Create CoGasJar.java

---
+++ CoGasJar.java
@@ -0,0 +1,13 @@
+public class CoGasJar {
+    public CoGasJar() {
+        display();
+    }
+    public void display(){
+        System.out.println(" You just knocked down a Carbon Monoxide Gas Jar, it will fill the room\n" +
+                "in five minutes.Unless you find a way out, you'll be at a fatal risk.");
+        System.out.println("**********************************************************************");
+    }
+    public void tryOperation(){
+        System.out.println("The mistake was already made, you cannot do anything to this jar, just find your way out.");
+    }
+}
