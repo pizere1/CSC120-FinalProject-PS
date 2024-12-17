333d8e1955a0076502db23bcba6bf2c154ce08a4
Swikriti Dumre <sdumre@smith.edu>
2024-12-17 14:20:34 -0600

    with audio file and sec guard

--- AlarmRoom.java
+++ AlarmRoom.java
@@ -1,4 +1,5 @@
-import java.io.File;
+
+import java.net.URL;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
@@ -27,16 +28,39 @@ public class AlarmRoom extends Room {
 /*
 *The function to play the alarm sounds
 */
-    public void playSound() {
-        try {
-            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("/Users/sdumre/Downloads/mixkit-facility-alarm-sound-999.wav").getAbsoluteFile());
-            Clip clip = AudioSystem.getClip();
-            clip.open(audioInputStream);
-            clip.start();
-        } catch(Exception ex) {
-            System.out.println("Error with playing sound.");
-            ex.printStackTrace();
+;
+
+
+
+public void playSound() {
+    try {
+        // Use getResource to load the file from the classpath
+        URL soundFile = getClass().getClassLoader().getResource("alarm-sound.wav");
+
+        if (soundFile == null) {
+            System.out.println("Sound file not found!");
+            return;
         }
+
+        AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(soundFile);
+        Clip clip = AudioSystem.getClip();
+        clip.open(audioInputStream);
+        clip.start();
+    } catch (Exception ex) {
+        System.out.println("Error playing sound: " + ex.getMessage());
     }
+}
+
+    // public void playSound() {
+        // try {
+            // AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("/Users/sdumre/Downloads/mixkit-facility-alarm-sound-999.wav").getAbsoluteFile());
+            // Clip clip = AudioSystem.getClip();
+            // clip.open(audioInputStream);
+            // clip.start();
+        // } catch(Exception ex) {
+            // System.out.println("Error with playing sound.");
+            // ex.printStackTrace();
+        // }
+    // }
 
 }
