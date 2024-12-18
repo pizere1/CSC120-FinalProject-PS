c057882127050e8de0f90bd4525b9a09fbe7fbb2
Peace Izere <pizere@smith.edu>
2024-12-17 20:43:02 -0500

    Update Securityguard.java with comments

--- Securityguard.java
+++ Securityguard.java
@@ -1,17 +1,30 @@
-public class Securityguard extends People { 
-
+public class Securityguard extends People {
+    /**
+     * Attributes
+     */
     public String operation;
 
+    /**
+     * Constructor the Security guard class
+     * @param name takes in their name
+     * @param age takes in their age
+     */
     public Securityguard (String name, int age){
         super(name, age);
-       String operation = "Takes people back to Jail";
+        String operation = "Takes people back to Jail";
     }
 
+    /**
+     * Function to catch the player
+     */
     public void catchPlayer(){
-          System.out.println("You're caught. The security guards are taking you back to the prison. No way to escape. Game Over.");
-          //break;
-        }
+        System.out.println("You're caught. The security guards are taking you back to the prison. No way to escape. Game Over.");
+        //break;
+    }
 
+    /**
+     * Function to print their operation
+     */
     public void operation(){
         System.out.println(operation);
     }
