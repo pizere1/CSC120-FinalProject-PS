88b78d1dd9edaff3c5038a1acfbb1f042cfbe4ac
Peace Izere <pizere@smith.edu>
2024-12-17 20:30:32 -0500

    Update People.java with comments

--- People.java
+++ People.java
@@ -9,21 +9,29 @@ public class People{
     private String name;
     private int age;
 
-
+    /**
+     * Constructor for people
+     * @param name takes in their name
+     * @param age takes in their age
+     */
     public People(String name, int age){
         this.name = name;
         this.age = age;
     }
 
+    /**
+     * Function for getting the person's name
+     * @return their name
+     */
     public String getName(){
         return this.name;
     }
 
+    /**
+     * Function for getting their age
+     * @return their age
+     */
     public int getAge(){
         return this.age;
     }
-    //public static void main(String[] args){
-       // People Prisoner = new People("Luva", 46, "Prisoner");
-
-    //}
 }
