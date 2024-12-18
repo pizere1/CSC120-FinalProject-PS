2ada5a9541eb9f4149b2d613a8d8f40230e7047c
Peace Izere <pizere@smith.edu>
2024-12-17 20:41:19 -0500

    Update RoomMapper.java with comments

--- RoomMapper.java
+++ RoomMapper.java
@@ -5,12 +5,18 @@ import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 
 public class RoomMapper {
+    /**
+     * Attributes
+     */
     private HashMap<String, String> roomMap;
     EscapeRoom es=new EscapeRoom("ER",4,-2,-2,12);
 
+    /**
+     * Constructor for the RoomMapper class
+     */
     public RoomMapper() {
         roomMap = new HashMap<>();
-    
+
     }
 
     /**
@@ -25,26 +31,40 @@ public class RoomMapper {
         roomMap.put(key, roomName);
     }
 
-    // Get the room name for a given coordinate
+    /**
+     * Function to get a location of given coordinates
+     * @param x takes in the x coordinate
+     * @param y takes in the y coordinate
+     * @param z takes in the z coordinate
+     * @return the location or walls if there is no specific feature
+     */
     public String getLocation(int x, int y, int z) {
         String key = generateKey(x, y, z);
         return roomMap.getOrDefault(key, "Walls"); // Return room name or default message
     }
 
+    /**
+     * Function to evealute the current location
+     * @param x takes in the x coordinate
+     * @param y takes in the y coordinate
+     * @param z takes in the z coordinate
+     * @param mapper takes in an instance of roommapper
+     * @param cellMate takes in an instance of cellmate
+     */
     public void evaluateLocation(int x, int y, int z, RoomMapper mapper, CellMate cellMate) {
         String location = mapper.getLocation(x, y, z);
-    
+
         switch (location) {
             case "Hallway in front of the cellmate at cell 12, End of Hallway on South":
                 System.out.println("You see your cellmate in the cell nearby.");
                 System.out.println("You can type 'speak' to talk to him.");
                 break;
-    
+
             case "AR":
                 System.out.println("An alarm blares loudly as you enter the room!");
                 this.playSound();
                 break;
-    
+
             case "ER":
                 System.out.println("Congratulations! You've reached the escape room.");
                 es.roomStructure();
@@ -52,12 +72,12 @@ public class RoomMapper {
 
             case "Walls":
                 System.out.println("");
-    
+
             default:
                 System.out.println("");
                 break;
         }
-    }    
+    }
 
     public void lookAround(int x, int y, int z){
         System.out.println("\n Nearby Locations:\n");
@@ -67,27 +87,42 @@ public class RoomMapper {
         System.out.println("West: " + getLocation(x + 2, y, z));
     }
 
-
-
+    /**
+     * function to play sound
+     */
     public void playSound() {
         try {
-        AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("/Users/sdumre/Downloads/mixkit-facility-alarm-sound-999.wav").getAbsoluteFile());
-        Clip clip = AudioSystem.getClip();
-        clip.open(audioInputStream);
-        clip.start();
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("/Users/sdumre/Downloads/mixkit-facility-alarm-sound-999.wav").getAbsoluteFile());
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioInputStream);
+            clip.start();
         } catch(Exception ex) {
             System.out.println("Error with playing sound.");
-           ex.printStackTrace();
-     }
-    }   
+            ex.printStackTrace();
+        }
+    }
 
-
+    /**
+     * function to check for a clue from the cellmate
+     * @param x takes in the x location
+     * @param y takes in the y location
+     * @param z takes in the z location
+     * @param cellMate takes in an instance of a cellmate
+     */
     public void clueCheck(int x, int y, int z, CellMate cellMate){
         if (x == 2 && y == -2 && z == 0){
             cellMate.cellMateInfo();
             System.out.println("You should consider speaking to him.\n");
         }
     }
+
+    /**
+     * Function to generate a key for the current location to use in a map
+     * @param x takes in an x coordinate
+     * @param y takes in a y coordinate
+     * @param z takes in a z coordinate
+     * @return the coordinates concatenated
+     */
     private String generateKey(int x, int y, int z) {
         return x + "," + y + "," + z; // changes coordinates to string
     }
@@ -96,8 +131,9 @@ public class RoomMapper {
         RoomMapper mapper = new RoomMapper();
         CellMate cellMate = new CellMate("RamHari", 14, 621);
 
-
-        // Add rooms
+        /**
+         * Adding rooms to the RoomMapper instance
+         */
         mapper.addRoom("Prison Cell", 0, 0, 0); //1
         mapper.addRoom("Hallway in front of cell 11", 2, 0, 0);// 2
         mapper.addRoom("Hallway in front of the cellmate at cell 12, End of Hallway on South", 2, -2, 0);//3
