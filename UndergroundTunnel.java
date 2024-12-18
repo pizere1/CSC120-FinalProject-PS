b438a370b4fb99a13039b0a50900415c414a792d
Peace Izere <pizere@smith.edu>
2024-12-17 20:58:38 -0500

    Update UndergroundTunnel.java with changes

--- UndergroundTunnel.java
+++ UndergroundTunnel.java
@@ -1,16 +1,32 @@
 public class UndergroundTunnel {
+    /**
+     * Attributes
+     */
     boolean opened=false;
+
+    /**
+     * Constructor for the underground Tunnel class
+     */
     public UndergroundTunnel() {
     }
+
+    /**
+     * Function for opening the underground tunnel
+     * @param item takes in the item to choose
+     */
     public void open(String item){
         if(item.equals("old crowbar")){
             System.out.println("The floor block is removed. There are downward steps.");
-         this.opened = true;
+            this.opened = true;
         } else {
             System.out.println(item + " cannot help you do that.");
             this.opened = false;
         }
     }
+
+    /**
+     * Function to descend into the tunnel
+     */
     public void descend(){
         if(opened==true){
             System.out.println("You descend through the square opening and climb down the steps .");
@@ -19,6 +35,10 @@ public class UndergroundTunnel {
             System.out.println("Sorry. You don't have anywhere to descend to");
         }
     }
+
+    /**
+     * Function for ending the game
+     */
     public void ending(){
         System.out.println("****************YAY!! YOU WON******************.");
 
